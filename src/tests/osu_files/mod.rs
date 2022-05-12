b800a2eae136e8f6b8fc183e668d79f159919168
Eddio0141 <eddio0141@gmail.com>
2022-05-12 01:18:08 +0100

    more files

--- src/tests/osu_files/mod.rs
+++ src/tests/osu_files/mod.rs
@@ -2,10 +2,12 @@ use crate::osu_file::OsuFile;
 
 #[test]
 fn all_files_parse_back() {
-    let v3 = include_str!("./files/v3.osu");
-    let v4 = include_str!("./files/v4.osu");
+    // let v3 = include_str!("./files/v3.osu");
+    // let v4 = include_str!("./files/v4.osu");
+    // let v5 = include_str!("./files/v5.osu");
+    let v14 = include_str!("./files/v14.osu");
 
-    let files = vec![v3, v4];
+    let files = vec![v14];
 
     for file in files {
         let osu_file = file.parse::<OsuFile>().unwrap();
