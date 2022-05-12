99b4136382039e5e8099a0e68a73ad7a81314086
Eddio0141 <eddio0141@gmail.com>
2022-05-12 12:41:16 +0100

    nvm removed leading ws function

--- src/parsers.rs
+++ src/parsers.rs
@@ -11,14 +11,14 @@ use nom::{
     IResult,
 };
 
-pub fn leading_ws<'a, F: 'a, O, E: ParseError<&'a str>>(
-    inner: F,
-) -> impl FnMut(&'a str) -> IResult<&'a str, O, E>
-where
-    F: FnMut(&'a str) -> IResult<&'a str, O, E>,
-{
-    preceded(multispace0, inner)
-}
+// pub fn leading_ws<'a, F: 'a, O, E: ParseError<&'a str>>(
+//     inner: F,
+// ) -> impl FnMut(&'a str) -> IResult<&'a str, O, E>
+// where
+//     F: FnMut(&'a str) -> IResult<&'a str, O, E>,
+// {
+//     preceded(multispace0, inner)
+// }
 
 pub fn trailing_ws<'a, F: 'a, O, E: ParseError<&'a str>>(
     inner: F,
