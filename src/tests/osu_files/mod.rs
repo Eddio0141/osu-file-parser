85d9475ef50c7245cce3b37457991bc2c6abaf50
Eddio0141 <eddio0141@gmail.com>
2022-05-14 07:04:07 +0100

    added test file v9.osu

--- src/tests/osu_files/mod.rs
+++ src/tests/osu_files/mod.rs
@@ -8,10 +8,11 @@ use pretty_assertions::assert_eq;
 fn all_files_parse_back() {
     // let v3 = include_str!("./files/v3.osu");
     // let v4 = include_str!("./files/v4.osu");
-    let v5 = include_str!("./files/v5.osu");
+    // let v5 = include_str!("./files/v5.osu");
+    let v9 = include_str!("./files/v9.osu");
     let v14 = include_str!("./files/v14.osu");
 
-    let files = vec![v5, v14];
+    let files = vec![v9, v14];
 
     for file in files {
         let file = file.replace("\r\n", "\n");
