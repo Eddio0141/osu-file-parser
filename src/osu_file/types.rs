e46d37d6a75a84e3a174ec4c42b6a3ead9fb2535
Eddio0141 <eddio0141@gmail.com>
2022-05-14 04:39:48 +0100

    fixed error type

--- src/osu_file/types.rs
+++ src/osu_file/types.rs
@@ -18,36 +18,32 @@ impl Default for Position {
     }
 }
 
+#[derive(Debug)]
 pub struct Error<E> {
-    /// Line number of the error.
-    pub line_number: usize,
+    /// Line index of the error.
+    pub line_index: usize,
     /// The error.
     pub error: E,
-}
-
-impl<E> Debug for Error<E>
-where
-    E: Debug,
-{
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        self.error.fmt(f)
-    }
 }
 
 impl<E> Error<E> {
     /// Returns a pretty error message with the affected line and the error.
     /// - Expensive than showing line number and error with the `Display` trait, as this iterates over the lines of the file input string.
+    /// - Slightly cheaper alternative is to use the `Display` trait.
     pub fn display_error_with_line(&self, file_input: &str) -> String
     where
         E: std::fmt::Display,
     {
-        let line = file_input.lines().nth(self.line_number).unwrap_or_default();
+        let line = file_input.lines().nth(self.line_index).unwrap_or_default();
 
-        format!("Line {}: {}, {}", self.line_number + 1, line, self.error)
+        format!("Line {}: {}, {}", self.line_index + 1, line, self.error)
     }
 
     pub fn from_err_with_line(error: E, line_number: usize) -> Self {
-        Self { line_number, error }
+        Self {
+            line_index: line_number,
+            error,
+        }
     }
 
     /// Combines other `Error` with a higher level error and line number of where it was being processed.
@@ -56,7 +52,7 @@ impl<E> Error<E> {
         E2: From<E>,
     {
         Error {
-            line_number: self.line_number + line_number,
+            line_index: self.line_index + line_number,
             error: E2::from(self.error),
         }
     }
@@ -71,7 +67,7 @@ impl<E> Error<E> {
         match inner {
             Ok(ok) => Ok(ok),
             Err(err) => Err(Error {
-                line_number: line_number + err.line_number,
+                line_index: line_number + err.line_index,
                 error: err.error.into(),
             }),
         }
@@ -83,7 +79,7 @@ where
     E: std::fmt::Display,
 {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "Line {}, {}", self.line_number + 1, self.error)
+        write!(f, "Line {}, {}", self.line_index + 1, self.error)
     }
 }
 
@@ -91,7 +87,7 @@ impl<E> From<E> for Error<E> {
     /// Error from parsing a single line can be converted to a `ParseError` directly.
     fn from(error: E) -> Self {
         Self {
-            line_number: 0,
+            line_index: 0,
             error,
         }
     }
