2247237abd40279fecc65975bcacaae439ee87fa
Eddio0141 <eddio0141@gmail.com>
2022-05-16 05:35:22 +0100

    version trait for use later

--- src/osu_file/types.rs
+++ src/osu_file/types.rs
@@ -108,4 +108,78 @@ impl<E> From<E> for Error<E> {
     }
 }
 
-pub trait V14 {}
+pub trait Version {
+    type ParseError;
+
+    fn from_str_v3(s: &str) -> std::result::Result<Self, Self::ParseError>
+    where
+        Self: Sized;
+
+    fn to_string_v3(&self) -> String;
+
+    fn from_str_v4(s: &str) -> std::result::Result<Self, Self::ParseError>
+    where
+        Self: Sized;
+
+    fn to_string_v4(&self) -> String;
+
+    fn from_str_v5(s: &str) -> std::result::Result<Self, Self::ParseError>
+    where
+        Self: Sized;
+
+    fn to_string_v5(&self) -> String;
+
+    fn from_str_v6(s: &str) -> std::result::Result<Self, Self::ParseError>
+    where
+        Self: Sized;
+
+    fn to_string_v6(&self) -> String;
+
+    fn from_str_v7(s: &str) -> std::result::Result<Self, Self::ParseError>
+    where
+        Self: Sized;
+
+    fn to_string_v7(&self) -> String;
+
+    fn from_str_v8(s: &str) -> std::result::Result<Self, Self::ParseError>
+    where
+        Self: Sized;
+
+    fn to_string_v8(&self) -> String;
+
+    fn from_str_v9(s: &str) -> std::result::Result<Self, Self::ParseError>
+    where
+        Self: Sized;
+
+    fn to_string_v9(&self) -> String;
+
+    fn from_str_v10(s: &str) -> std::result::Result<Self, Self::ParseError>
+    where
+        Self: Sized;
+
+    fn to_string_v10(&self) -> String;
+
+    fn from_str_v11(s: &str) -> std::result::Result<Self, Self::ParseError>
+    where
+        Self: Sized;
+
+    fn to_string_v11(&self) -> String;
+
+    fn from_str_v12(s: &str) -> std::result::Result<Self, Self::ParseError>
+    where
+        Self: Sized;
+
+    fn to_string_v12(&self) -> String;
+
+    fn from_str_v13(s: &str) -> std::result::Result<Self, Self::ParseError>
+    where
+        Self: Sized;
+
+    fn to_string_v13(&self) -> String;
+
+    fn from_str_v14(s: &str) -> std::result::Result<Self, Self::ParseError>
+    where
+        Self: Sized;
+
+    fn to_string_v14(&self) -> String;
+}
