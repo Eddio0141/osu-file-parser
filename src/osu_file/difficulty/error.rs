128880857313a8cdf524347e9e9e00f4cf64f6ae
Eddio0141 <eddio0141@gmail.com>
2022-05-17 03:21:15 +0100

    fixed message

--- src/osu_file/difficulty/error.rs
+++ src/osu_file/difficulty/error.rs
@@ -5,16 +5,16 @@ use thiserror::Error;
 #[derive(Debug, Error)]
 /// Error used when there was a problem parsing the `Difficulty` section.
 pub enum ParseError {
-    /// A Field in `General` failed to parse as a `Integer`.
+    /// A Field in `Difficulty` failed to parse as a `Integer`.
     #[error(transparent)]
     ParseIntError(#[from] ParseIntError),
-    /// A Field in `General` failed to parse as a `Decimal`.
+    /// A Field in `Difficulty` failed to parse as a `Decimal`.
     #[error(transparent)]
     ParseDecimalError(#[from] rust_decimal::Error),
     /// When the line isn't in a `key: value` format.
     #[error("Invalid colon set, expected format of `key: value`")]
     InvalidColonSet,
     /// Invalid key name was used.
-    #[error("The key doesn't exist in `General`")]
+    #[error("The key doesn't exist in `Difficulty`")]
     InvalidKey,
 }
