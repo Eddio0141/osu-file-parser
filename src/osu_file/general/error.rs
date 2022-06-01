0798f370aa2df629d9435d215bd9e537bcc5d1bf
Eddio0141 <eddio0141@gmail.com>
2022-06-01 23:18:23 +0100

    new error for invalid version

--- src/osu_file/general/error.rs
+++ src/osu_file/general/error.rs
@@ -18,13 +18,13 @@ pub enum ParseError {
     ParseZeroOneBoolError(#[from] ParseZeroOneBoolError),
     /// A field in `General` failed to parse as a `CountdownSpeed`.
     #[error(transparent)]
-    CountdownSpeedParseError(#[from] CountdownSpeedParseError),
+    CountdownSpeedParseError(#[from] ParseCountdownSpeedError),
     /// A field in `General` failed to parse as an enum string.
     #[error(transparent)]
     StrumParseError(#[from] strum::ParseError),
     /// A field in `General` failed to parse as a `GameMode`.
     #[error(transparent)]
-    GameModeParseError(#[from] GameModeParseError),
+    GameModeParseError(#[from] ParseGameModeError),
     /// When the line isn't in a `key: value` format.
     #[error("Invalid colon set, expected format of `key: value`")]
     InvalidColonSet,
@@ -34,11 +34,14 @@ pub enum ParseError {
     /// Duplicate field in `General` were found.
     #[error("Duplicate field were found in `General`")]
     DuplicateField,
+    /// Field doesn't exist in version.
+    #[error("Field doesn't exist in version")]
+    InvalidVersion,
 }
 
 /// Error used when there's an error parsing the string as enum.
 #[derive(Debug, Error)]
-pub enum GameModeParseError {
+pub enum ParseGameModeError {
     /// Error when the `GameMode` is not a valid enum.
     #[error("Unknown `GameMode`")]
     UnknownType,
@@ -49,7 +52,7 @@ pub enum GameModeParseError {
 
 /// Error used when there's an error parsing the string as enum
 #[derive(Debug, Error)]
-pub enum CountdownSpeedParseError {
+pub enum ParseCountdownSpeedError {
     /// The integer value is an unknown `CountdownSpeed` type.
     #[error("The integer value is an unknown `CountdownSpeed` type")]
     UnknownType,
