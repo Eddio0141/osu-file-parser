6624a248190496993d3e4f11fb6345b362b1abfd
Eddio0141 <eddio0141@gmail.com>
2022-08-10 22:51:52 +0100

    split assert_eq_osu_str func into parts

--- src/lib.rs
+++ src/lib.rs
@@ -6,51 +6,60 @@ pub mod osu_file;
 pub use osu_file::*;
 mod parsers;
 
-/// Function to test equality of two osu file strings.
+/// Trims the given osu file string into something that can be tested for equality.
 /// - Ignores all empty lines and key value pair's spacing between the key and comma.
 /// - Deletes `\u{feff}` characters.
+pub fn osu_str_trimmer(s: &str) -> String {
+    s.lines()
+        .filter_map(|s| {
+            // remove the weird \u{feff} characters
+            let s = s.trim().replace('\u{feff}', "");
+
+            if s.is_empty() {
+                return None;
+            }
+
+            if s.contains(':') {
+                let mut header = true;
+                let mut value = false;
+
+                return Some(
+                    s.chars()
+                        .filter(|c| {
+                            if !header && !value && *c == ':' {
+                                header = false;
+                                value = true;
+                                return true;
+                            }
+
+                            if header && c.is_whitespace() {
+                                return false;
+                            }
+
+                            true
+                        })
+                        .collect::<String>(),
+                );
+            }
+
+            Some(s)
+        })
+        .collect::<Vec<_>>()
+        .join("\n")
+}
+
+/// Tests equality of two osu file strings.
+pub fn osu_str_eq<L: AsRef<str>, R: AsRef<str>>(left: L, right: R) -> bool {
+    let left = osu_str_trimmer(left.as_ref());
+    let right = osu_str_trimmer(right.as_ref());
+
+    left == right
+}
+
+/// Asserts that two osu file strings are equal.
 pub fn assert_eq_osu_str<L: AsRef<str>, R: AsRef<str>>(left: L, right: R) {
-    let trimmer = |s: &str| {
-        s.lines()
-            .filter_map(|s| {
-                // remove the weird \u{feff} characters
-                let s = s.trim().replace('\u{feff}', "");
+    let left = osu_str_trimmer(left.as_ref());
+    let right = osu_str_trimmer(right.as_ref());
 
-                if s.is_empty() {
-                    return None;
-                }
-
-                if s.contains(':') {
-                    let mut header = true;
-                    let mut value = false;
-
-                    return Some(
-                        s.chars()
-                            .filter(|c| {
-                                if !header && !value && *c == ':' {
-                                    header = false;
-                                    value = true;
-                                    return true;
-                                }
-
-                                if header && c.is_whitespace() {
-                                    return false;
-                                }
-
-                                true
-                            })
-                            .collect::<String>(),
-                    );
-                }
-
-                Some(s)
-            })
-            .collect::<Vec<_>>()
-            .join("\n")
-    };
-
-    let left = trimmer(left.as_ref());
-    let right = trimmer(right.as_ref());
-
-    pretty_assertions::assert_eq!(left, right);
+    pretty_assertions::assert_eq!(left, right)
 }
