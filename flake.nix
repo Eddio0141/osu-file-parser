15fd47c126b89920179a97577d943d9fbb563c37
Eddio0141 <eddio0141@gmail.com>
2024-07-04 21:22:54 +0000

    added dev env

---
+++ flake.nix
@@ -0,0 +1,45 @@
+{
+  inputs = {
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
+
+  outputs = { flake-parts, nixpkgs, rust-overlay, ... } @ inputs:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+      ];
+      perSystem = { self', inputs', system, pkgs, ... }:
+        let
+          rust-doc = pkgs.writeShellApplication {
+            name = "rust-doc";
+            text = ''
+              xdg-open "${inputs'.fenix.packages.stable.rust-docs}/share/doc/rust/html/index.html"
+            '';
+          };
+        in
+        {
+          _module.args.pkgs = import nixpkgs {
+            inherit system;
+            overlays = [
+              (import rust-overlay)
+            ];
+          };
+
+          devShells.default = pkgs.mkShell {
+            packages = with pkgs; [
+              rust-bin.stable.latest.default
+              rust-doc
+            ];
+          };
+        };
+    };
+}
